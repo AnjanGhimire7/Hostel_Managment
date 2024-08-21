bd1d747fce7be7d658f4bcb9e7de1ed15a3a86bb
AnjanGhimire7 <anjanghimire876@gmail.com>
2024-08-21 21:19:07 +0545

    fixing some bugs

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
-import sanitizer from "sanitizer";
+import sanitize from "express-mongo-sanitize";
 import xss from "xss";
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(
   })
 );
 app.use(helmet());
-app.use(sanitizer());
+app.use(sanitize());
 app.use(xss());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
